1340a0232f7bda879937b0060f54a6998f90b6d6
yusuf <yusuf@endpoint-labs.com>
2020-08-06 17:18:05 +0300

    Test some icons on home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,16 +5,25 @@
     <CustomText class="a2" tag="p" size="small">Merhaba Dunya</CustomText>
     <CustomText class="a3" tag="p" size="xsmall">Merhaba Dunya</CustomText>
     <CustomText class="a4" tag="p" size="xxsmall">Merhaba Dunya</CustomText>
+    <IconHome />
+    <IconLike />
+    <IconComment />
   </div>
 </template>
 
 <script>
 import CustomText from '@/components/CustomText'
+import IconHome from '../icons/home.svg'
+import IconLike from '../icons/post-like.svg'
+import IconComment from '../icons/post-comment.svg'
 
 export default {
   name: 'Home',
   components: {
-    CustomText
+    CustomText,
+    IconHome,
+    IconLike,
+    IconComment
   }
 }
 </script>
