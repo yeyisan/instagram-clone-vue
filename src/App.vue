6f18866a428397c745d933c1df7e6d629a3fa216
Yusuf <yusuf@endpoint-labs.com>
2020-08-06 21:39:57 +0300

    Add direct and explore pages to router link

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/direct">Direct</router-link> |
+      <router-link to="/explore">Explore</router-link> |
       <router-link to="/profile">Profile</router-link>
     </div>
     <router-view />
