d797adf5ffcafd8f83ede5ead8b43d5c735a0b01
Yusuf <yusuf@endpoint-labs.com>
2020-08-06 21:39:24 +0300

    Create new explore and direct pages

---
+++ src/views/direct/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="direct">
+    <h1>Direct</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Direct',
+  components: {}
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/explore/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="explore">
+    <h1>Explore</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Explore',
+  components: {}
+}
+</script>
+
+<style scoped></style>
