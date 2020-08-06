d718a9d51b8fa31ef25e9fdffdbe47fd19d522e8
yusuf <yusuf@endpoint-labs.com>
2020-08-06 16:11:29 +0300

    Use first component to testing typography

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText class="a1" tag="p">Merhaba Dunya</CustomText>
+    <CustomText class="a2" tag="p" size="small">Merhaba Dunya</CustomText>
+    <CustomText class="a3" tag="p" size="xsmall">Merhaba Dunya</CustomText>
+    <CustomText class="a4" tag="p" size="xxsmall">Merhaba Dunya</CustomText>
   </div>
 </template>
 
 <script>
+import CustomText from '@/components/CustomText'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    CustomText
+  }
 }
 </script>
 
-<style>
-h1 {
-  color: red;
-  @media (--t) {
-    color: blue;
-  }
+<style scoped>
+.a1 {
+  color: rgb(var(--a72));
+}
+.a2 {
+  color: rgb(var(--a97));
+}
+.a3 {
+  color: rgb(var(--b6a));
+}
+.a4 {
+  color: rgb(var(--b38));
 }
 </style>
