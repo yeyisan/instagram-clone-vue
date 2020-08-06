324dadd84f6e4aa83f7d60a164ec2705de299fc4
Yusuf <yusuf@endpoint-labs.com>
2020-08-06 23:03:02 +0300

    Redesign header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,18 +1,90 @@
 <template>
-  <header>
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/direct">Direct</router-link>
-      |
-      <router-link to="/explore">Explore</router-link>
-      |
-      <router-link to="/profile">Profile</router-link>
-    </div>
+  <header class="header">
+    <Container class="header-inner">
+      <router-link to="/" class="logo">
+        Instagram
+      </router-link>
+
+      <form class="search">
+        <input type="text" placeholder="Search" />
+      </form>
+
+      <nav class="navigation">
+        <router-link to="/">
+          <IconHomeFill v-if="$route.name === 'Home'" />
+          <IconHome v-else />
+        </router-link>
+
+        <router-link to="/direct">
+          <IconDirectFill v-if="$route.name === 'Direct'" />
+          <IconDirect v-else />
+        </router-link>
+
+        <router-link to="/explore">
+          <IconExploreFill v-if="$route.name === 'Explore'" />
+          <IconExplore v-else />
+        </router-link>
+
+        <router-link to="/profile"> Profile</router-link>
+      </nav>
+    </Container>
   </header>
 </template>
 <script>
+import IconHome from '@/icons/home.svg'
+import IconDirect from '@/icons/direct-message.svg'
+import IconExplore from '@/icons/fing-people.svg'
+import IconHomeFill from '@/icons/home-fill.svg'
+import IconDirectFill from '@/icons/direct-message-fill.svg'
+import IconExploreFill from '@/icons/find-people-fill.svg'
+import Container from '@/components/Container'
 export default {
-  name: 'MainHeader'
+  name: 'MainHeader',
+  components: {
+    Container,
+    IconHome,
+    IconDirect,
+    IconExplore,
+    IconHomeFill,
+    IconDirectFill,
+    IconExploreFill
+  }
 }
 </script>
+
+<style scoped>
+.header {
+  border-bottom: 1px solid rgb(var(--b6a));
+}
+.header-inner {
+  height: 60px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+
+}
+.logo {
+  font-size: 1.6rem;
+  font-weight: bold;
+}
+.search {
+  text-align: center;
+  input {
+    width: 215px;
+    height: 28px;
+    border: 1px solid rgb(var(--b6a));
+    background-color: rgb(var(--b3f));
+    border-radius: 4px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+}
+.navigation {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  a {
+    margin-left: 20px;
+  }
+}
+</style>
